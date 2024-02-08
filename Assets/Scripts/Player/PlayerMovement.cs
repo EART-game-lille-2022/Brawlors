9d2d23eb7c57cb40ee4ed232cb874cf2c293f266
D'alessandro Ethan <roger.paldi@yahoo.com>
2024-02-08 12:33:56 +0100

    Levitator et GrapUngrap qui fonctionne

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,23 +7,10 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 moveDirection;
     private Rigidbody2D rb2D;
     [SerializeField] private float moveSpeed;
-    [SerializeField][Tooltip("est-ce que le player est au dessus du sol ?")] private bool isOnGround = true;
-
-    [Header("Animation Values")]
-    [SerializeField] private AnimationCurve wavingCurve;
-    [SerializeField] private float wavingValue;
-    [SerializeField] private float frequency;
-    [SerializeField] private float wavingForce;
 
     private void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate()
-    {
-        wavingValue = wavingCurve.Evaluate(Time.time * frequency) * wavingForce;
-        rb2D.velocity = new Vector2(rb2D.velocity.x ,rb2D.velocity.y + wavingValue);
     }
 
     public void DoMove(InputAction.CallbackContext context)
