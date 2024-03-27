3d718b7b6270444a3b3108f8d83e915093835947
D'alessandro Ethan <roger.paldi@yahoo.com>
2024-03-27 09:17:09 +0100

    Portal added

--- Assets/Scripts/Environnement/WarpingPortal.cs
+++ Assets/Scripts/Environnement/WarpingPortal.cs
@@ -1,26 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WarpingPortal : MonoBehaviour
 {
-    public Transform actualPosition;
     public ObjectColorProperties actualColorProperties;
 
     public static Dictionary<ScriptableObjectColor, List<WarpingPortal>> portalsDictionnary = new Dictionary<ScriptableObjectColor, List<WarpingPortal>>();
 
     private void Start()
     {
-        actualPosition = transform;
         actualColorProperties = transform.GetComponent<ObjectColorProperties>();
         actualColorProperties.OnColorChange.AddListener(SetColor);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if (collision != null && collision.gameObject.CompareTag("Player"))
         {
-            
+            if(GetTarget() == null)
+            { return; }
+            GameObject player = collision.gameObject;
+            player.transform.position = new Vector3(GetTarget().transform.position.x, GetTarget().transform.position.y, GetTarget().transform.position.z);
         }
     }
 
@@ -30,29 +30,32 @@ public class WarpingPortal : MonoBehaviour
         if (lastColor == null) return null;
 
         List<WarpingPortal> _portalsList;
-        if (!portalsDictionnary.TryGetValue(lastColor, out _portalsList) || _portalsList.Count < 2 || !_portalsList.Contains(this)) 
+        if (!portalsDictionnary.TryGetValue(lastColor, out _portalsList) || _portalsList.Count < 2 || !_portalsList.Contains(this))
             return null;
 
         int index = _portalsList.IndexOf(this);
-        return _portalsList[(index + 1)%_portalsList.Count];
+        return _portalsList[(index + 1) % _portalsList.Count];
     }
 
     //Nello
     ScriptableObjectColor lastColor;
-    public void SetColor(ScriptableObjectColor scriptcolor) {
-        if(lastColor == scriptcolor) return;
+    public void SetColor(ScriptableObjectColor scriptcolor)
+    {
+        if (lastColor == scriptcolor) return;
 
-        if(lastColor != null) {
+        if (lastColor != null)
+        {
             portalsDictionnary[lastColor].Remove(this);
         }
 
         List<WarpingPortal> _portalsList;
-        if(!portalsDictionnary.TryGetValue(scriptcolor, out _portalsList)) {
+        if (!portalsDictionnary.TryGetValue(scriptcolor, out _portalsList))
+        {
             _portalsList = new List<WarpingPortal>();
             portalsDictionnary.Add(scriptcolor, _portalsList);
         }
 
-        if(!_portalsList.Contains(this)) 
+        if (!_portalsList.Contains(this))
             _portalsList.Add(this);
     }
 }
